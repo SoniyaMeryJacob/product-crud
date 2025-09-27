de797c28c08eda08623f4c4ad944e4f5f9febc7a
soniya <121152577+soniya2205@users.noreply.github.com>
2025-09-27 16:16:47 +0530

    main page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { queryClient } from "@/lib/queryClient";
 import "../app/globals.css";
 
 import type { AppProps } from "next/app";
-
+// Use QueryClientProvider to provide the client to your app
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
